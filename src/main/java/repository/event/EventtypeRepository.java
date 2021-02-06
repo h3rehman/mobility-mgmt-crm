9c586c0eea3c08675ee78b83575dd350334b4fff
RehmanH-P52 <h3rehman@gmail.com>
2021-02-05 23:10:25 -0600

    Added controllers for events pagination, sorting and filters.

--- src/main/java/repository/event/EventtypeRepository.java
+++ src/main/java/repository/event/EventtypeRepository.java
@@ -1,10 +1,22 @@
 package repository.event;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventtypeRepository extends JpaRepository<Eventtype, Long>{
 
 	public Eventtype findByeventTypeDesc (String eventTypeDesc);
+	
+	/**
+	 * The following query gives an exception:
+	 * com.fasterxml.jackson.databind.JsonMappingException: 
+	 * Null key for a Map not allowed in JSON (use a converting NullKeySerializer?)
+	 */
+	@Query("SELECT et.eventTypeId, et.eventTypeDesc FROM Eventtype et")
+	public List<Map<String, Object>> findAlleventTypes();
 }
--- src/test/java/repository/event/EventJpaTests.java
+++ src/test/java/repository/event/EventJpaTests.java
@@ -3,21 +3,49 @@ package repository.event;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import javax.sql.DataSource;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import application.Application;
 
+@EnableAutoConfiguration
 @SpringBootTest(classes = Application.class)
+@ExtendWith(SpringExtension.class)
 public class EventJpaTests {
 	
 	@Autowired
 	EventRepository eventRepository;
+	
+	@Autowired
+	EventtypeRepository eventTypeRepository;
+	
+	
+//	@Autowired
+//	DataSource dataSource;
+//	
+//	JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+//	EventJpaTests (DataSource dataSource){
+//		this.dataSource = dataSource;
+//		this.jdbcTemplate = new JdbcTemplate(dataSource);
+//	}
 	
 	@Test
 	public void testForAllEvents() {
@@ -37,5 +65,46 @@ public class EventJpaTests {
 //		List<Event> events = eventRepository.findAll();
 //		assertEquals("The Prestige", events.get(2).getEventName());
 //		assertEquals("Resource Fair", events.get(2).getEventType());
+	}
+	
+	@Test
+	public void testForEventFilterAndCount() {
+		
+		Long eventCountJPALong = eventRepository.count();
+		int eventCountJPA = eventCountJPALong.intValue();
+		
+		String sql = "SELECT COUNT(*) FROM Event";
+//		Integer eventCountJDBC = jdbcTemplate.queryForObject(sql, Integer.class);
+		
+		assertEquals(eventCountJPA, 26);
+		
+		Pageable pageable = PageRequest.of(0, eventCountJPA);
+		
+		LocalDateTime fromDate = null;
+		LocalDateTime toDate = null;
+		
+		String from = "2020-10-01 00:00:00";
+		String to = "2021-01-31 00:00:00";
+		
+		String sql2 = "SELECT * from EVENT e "
+					+ "WHERE e.startdatetime between ? AND ? "
+					+ "AND e.EventtypeID = ?";
+		
+//		List<Map<String, Object>> eventsJDBC = jdbcTemplate.queryForList(sql2, from, to, 1L);
+			
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		fromDate = LocalDateTime.parse(from, formatter);
+		toDate = LocalDateTime.parse(to, formatter);
+		
+		Eventtype eventType = eventTypeRepository.findByeventTypeDesc("Presentation");
+		
+		List <Eventtype> eventTypes = new ArrayList<Eventtype>();
+		eventTypes.add(eventType);
+		
+		Page<Event> eventsJPA = eventRepository.
+		findBystartDateTimeBetweenAndeventTypeIn(fromDate, toDate, eventTypes, pageable);
+		
+		assertEquals(eventsJPA.getTotalElements(), 5);
+		
 	}
 }
