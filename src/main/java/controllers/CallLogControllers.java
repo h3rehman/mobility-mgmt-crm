1be7c719aff6c1e49fe0b8f7bac56330cb05e434
RehmanH-P52 <h3rehman@gmail.com>
2021-04-28 17:34:06 -0500

    Added Call-Log: filters, sorting and pagination.

--- src/main/java/controllers/CallLogControllers.java
+++ src/main/java/controllers/CallLogControllers.java
@@ -1,10 +1,17 @@
 package controllers;
 
 import java.net.URI;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,6 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -23,6 +31,8 @@ import repository.calllog.CallLogRepository;
 import repository.event.presenter.Presenter;
 import repository.note.Note;
 import repository.note.NoteRepository;
+import repository.status.Status;
+import repository.status.StatusRepository;
 import services.CallLogService;
 
 @RestController
@@ -34,6 +44,9 @@ public class CallLogControllers {
 	
 	@Autowired
 	NoteRepository noteRepository;
+	
+	@Autowired
+	StatusRepository statusRepository;
 	
 	@Autowired
 	CallLogService callLogService;
@@ -101,4 +114,71 @@ public class CallLogControllers {
 					lastStatusId, requestType);
 		}
 	}
+	
+	@GetMapping("/call-logs-sorted-default")
+	public Page<CallLog> getMyDefaultCallLogs () {
+		final int pageNumber = 0;
+		final int pageElements = 10;
+		Pageable pageable = PageRequest.of(pageNumber, pageElements, Sort.by("createDate").descending());
+		Page<CallLog> callLogs = callLogRepository.findAll(pageable);
+		return callLogs;
+	}
+	
+	@GetMapping("/call-logs-filtered-sorted/{pageNumber}/{pageElements}/{fieldName}/{sortOrder}/{from}/{to}")
+	public Page<CallLog> getCustomCallLogs (@PathVariable Integer pageNumber, @PathVariable Integer pageElements, 
+			@PathVariable String fieldName, @PathVariable String sortOrder, @PathVariable String from, @PathVariable String to, 
+			@AuthenticationPrincipal Presenter user, @RequestParam(value="status", required=false) String [] callLogStatuses) {
+		
+		Pageable pageable = null;
+		Page<CallLog> callLogs = null;
+		
+		if (!fieldName.equalsIgnoreCase("null")) {
+			if (sortOrder.equalsIgnoreCase("asce") || sortOrder.equalsIgnoreCase("null") ) {
+				pageable = PageRequest.of(pageNumber, pageElements, Sort.by(fieldName).ascending());
+			}
+			else {
+				pageable = PageRequest.of(pageNumber, pageElements, Sort.by(fieldName).descending());
+			}
+		}
+		else { //default sort 
+			pageable = PageRequest.of(pageNumber, pageElements, Sort.by("createDate").descending());
+		}
+		
+		LocalDateTime fromLMDDate = null;
+		LocalDateTime toLMDDate = null;
+
+		if (!from.equalsIgnoreCase("null") && !to.equalsIgnoreCase("null")) {
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+			fromLMDDate = LocalDateTime.parse(from, formatter);
+			toLMDDate = LocalDateTime.parse(to, formatter);
+		}
+		
+		List<Status> statuses = new ArrayList<Status>();
+		if (callLogStatuses != null) {
+			for (int i=0; i<callLogStatuses.length; i++) {
+				Status status = statusRepository.findBystatusDesc(callLogStatuses[i]);
+				if (status != null) {
+					statuses.add(status);
+				}
+			}
+		}
+		
+		if (statuses.size() > 0 && fromLMDDate != null && toLMDDate != null) {
+			callLogs = callLogRepository.findByCreatedByAndStatusInAndLastModifiedDateBetween(user, statuses, fromLMDDate, toLMDDate, pageable);
+		}
+		
+		else if (statuses.size() > 0 && fromLMDDate == null && toLMDDate == null) {
+			callLogs = callLogRepository.findByCreatedByAndStatusIn(user, statuses, pageable);
+		}
+		
+		else if (statuses.size() < 1 && fromLMDDate != null && toLMDDate != null) {
+			callLogs = callLogRepository.findByCreatedByAndLastModifiedDateBetween(user, fromLMDDate, toLMDDate, pageable);
+		}
+		else {
+			callLogs = callLogRepository.findAll(pageable);
+		}
+		
+		return callLogs;
+	}
+	
 }
