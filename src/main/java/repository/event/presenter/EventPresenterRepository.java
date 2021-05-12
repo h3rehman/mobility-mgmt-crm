e771a50c189b684ea23a80b2278508b7fd66fb01
RehmanH-P52 <h3rehman@gmail.com>
2021-05-11 22:30:20 -0500

    Added controllers and pagination for My-events

--- src/main/java/repository/event/presenter/EventPresenterRepository.java
+++ src/main/java/repository/event/presenter/EventPresenterRepository.java
@@ -1,13 +1,77 @@
 package repository.event.presenter;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import repository.event.Event;
+import repository.event.Eventtype;
+import repository.status.Status;
+
+/**
+ * 
+ * @author Hamood
+ * Most of the methods have nested objects arising from the Event class.  
+ *
+ */
 
 @Repository
 public interface EventPresenterRepository extends JpaRepository<Eventpresenter, Long> {
 	
 	public Eventpresenter findByEventAndPresenter(Event event, Presenter presenter);
+	//Default query
+//	public List<Eventpresenter> findByPresenterAndEventStartDateTimeGreaterThanEqual(Presenter user, LocalDateTime dateTime);
+//	
+//	public List<Eventpresenter> findByPresenterAndEventEventTypeInAndEventStartDateTimeGreaterThanEqual(Presenter user, List<Eventtype> eventTypes, LocalDateTime dateTime);
+//	
+//	public List<Eventpresenter> findByPresenterAndEventEventTypeInAndEventLastStatusInAndEventStartDateTimeGreaterThanEqual(Presenter user, List<Eventtype> eventTypes, List<Status> lastStatuses, LocalDateTime dateTime);
+//	
+//	public List<Eventpresenter> findByPresenterAndEventLastStatusInAndEventStartDateTimeGreaterThanEqual(Presenter user, List<Status> lastStatuses, LocalDateTime dateTime);
+//		
+//	public List<Eventpresenter> findByPresenterAndEventStartDateTimeBetween(Presenter user, LocalDateTime fromDate, LocalDateTime toDate);
+//
+//	public List<Eventpresenter> findByPresenterAndEventStartDateTimeBetweenAndEventEventTypeIn(Presenter user, LocalDateTime fromDate, LocalDateTime toDate, List<Eventtype> eventTypes);
+//	
+//	public List<Eventpresenter> findByPresenterAndEventStartDateTimeBetweenAndEventLastStatusIn (Presenter user, LocalDateTime fromDate, LocalDateTime toDate, List<Status> lastStatuses);
+//	
+//	public List<Eventpresenter> findByPresenterAndEventStartDateTimeBetweenAndEventEventTypeInAndEventLastStatusIn(Presenter user, LocalDateTime fromDate, LocalDateTime toDate, List<Eventtype> eventTypes, List<Status> lastStatuses);
+//	
+//	public List<Eventpresenter> findByPresenterAndEventEventTypeIn(Presenter user, List<Eventtype> eventTypes);
+//	
+//	public List<Eventpresenter> findByPresenterAndEventEventTypeInAndEventLastStatusIn(Presenter user, List<Eventtype> eventTypes, List<Status> lastStatuses);
+//	
+//	public List<Eventpresenter> findByPresenterAndEventLastStatusIn(Presenter user, List<Status> lastStatuses);
+//	
+//	public List<Eventpresenter> findByPresenter(Presenter user);
+	
+	
+	//Default query
+	public Page<Eventpresenter> findByPresenterAndEventStartDateTimeGreaterThanEqual(Presenter user, LocalDateTime dateTime, Pageable pageable);
+	
+	public Page<Eventpresenter> findByPresenterAndEventEventTypeInAndEventStartDateTimeGreaterThanEqual(Presenter user, List<Eventtype> eventTypes, LocalDateTime dateTime, Pageable pageable);
+		
+	public Page<Eventpresenter> findByPresenterAndEventEventTypeInAndEventLastStatusInAndEventStartDateTimeGreaterThanEqual(Presenter user, List<Eventtype> eventTypes, List<Status> lastStatuses, LocalDateTime dateTime, Pageable pageable);
+		
+	public Page<Eventpresenter> findByPresenterAndEventLastStatusInAndEventStartDateTimeGreaterThanEqual(Presenter user, List<Status> lastStatuses, LocalDateTime dateTime, Pageable pageable);
+			
+	public Page<Eventpresenter> findByPresenterAndEventStartDateTimeBetween(Presenter user, LocalDateTime fromDate, LocalDateTime toDate, Pageable pageable);
+
+	public Page<Eventpresenter> findByPresenterAndEventStartDateTimeBetweenAndEventEventTypeIn(Presenter user, LocalDateTime fromDate, LocalDateTime toDate, List<Eventtype> eventTypes, Pageable pageable);
+		
+	public Page<Eventpresenter> findByPresenterAndEventStartDateTimeBetweenAndEventLastStatusIn (Presenter user, LocalDateTime fromDate, LocalDateTime toDate, List<Status> lastStatuses, Pageable pageable);
+		
+	public Page<Eventpresenter> findByPresenterAndEventStartDateTimeBetweenAndEventEventTypeInAndEventLastStatusIn(Presenter user, LocalDateTime fromDate, LocalDateTime toDate, List<Eventtype> eventTypes, List<Status> lastStatuses, Pageable pageable);
+		
+	public Page<Eventpresenter> findByPresenterAndEventEventTypeIn(Presenter user, List<Eventtype> eventTypes, Pageable pageable);
+		
+	public Page<Eventpresenter> findByPresenterAndEventEventTypeInAndEventLastStatusIn(Presenter user, List<Eventtype> eventTypes, List<Status> lastStatuses, Pageable pageable);
+	
+	public Page<Eventpresenter> findByPresenterAndEventLastStatusIn(Presenter user, List<Status> lastStatuses, Pageable pageable);
+	
+	public Page<Eventpresenter> findByPresenter(Presenter user, Pageable pageable);
 
 }
