01582f9b9887111048f9a3b2850691199f8fbcf3
RehmanH-P52 <h3rehman@gmail.com>
2021-07-08 13:38:19 -0500

    Added elasticsearch host/port config in YAML.

--- src/main/java/config/ElasticsearchRestClientConfig.java
+++ src/main/java/config/ElasticsearchRestClientConfig.java
@@ -6,9 +6,11 @@ import javax.annotation.PostConstruct;
 
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration.MaybeSecureClientConfigurationBuilder;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 
@@ -20,14 +22,26 @@ import repository.event.EventRepository;
 @Configuration
 public class ElasticsearchRestClientConfig extends AbstractElasticsearchConfiguration {
 
+	@Value("${elasticsearch.host}")
+	private String esHost;
+	
+	@Value("${elasticsearch.port}")
+	private String esPort;
+	
+	@Value("${elasticsearch.superuser.username}")
+	private String esSuperuser;
+	
+	@Value("${elasticsearch.superuser.password}")
+	private String esSUPassword;
 	
 	@Override
     @Bean
     public RestHighLevelClient elasticsearchClient() {
 
-        final ClientConfiguration clientConfiguration = ClientConfiguration.builder()  
-            .connectedTo("localhost:9200")
-//            .usingSsl()
+        final ClientConfiguration clientConfiguration = ((MaybeSecureClientConfigurationBuilder) ClientConfiguration.builder()  
+            .connectedTo(esHost + ":" + esPort)
+            .withBasicAuth(esSuperuser, esSUPassword))
+            .usingSsl()
             .build();
 
         return RestClients.create(clientConfiguration).rest();                         
